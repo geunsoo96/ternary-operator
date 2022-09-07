56beb9cbdd57bb69c4872fc4aa597b3bcb64a3e3
Kimgeunsoo <geunsoo1106@gmail.com>
2022-09-07 10:59:59 +0900

    삼항 연산자 공부

---
+++ study.js
@@ -0,0 +1,17 @@
+// 삼항 연산자(ternary-operator)
+let operandOne = "test";
+let operandTwo = 4;
+// operand(용어): 피연산자
+let result = operandOne.length === operandTwo ? "hello" : "bye";
+// if문과 동일
+// 문장 뜻: 만약 operandOne의 문장배열과 operandTwo의 숫자요소 4가 동일 하다면 hello라는 문장 출력 else로는 "bye"출력
+console.log(result);
+
+let geunsooAge = 27;
+// 근수 나이 스물일곱살
+let beverage = geunsooAge < 30 ? "영계" : "할아범";
+// 변수 = 만약 내나이가 30살보다 어리면 "영계"
+// 더 많으면 "할아범" 출력
+console.log(beverage);
+
+// 써본 소감: 난 if문이 더 쉬운것같다.
